04dec86bba1f49d39110de0dbf6150b057213a1a
Suprabhatgit <Ayxxnyrr@gmail.com>
2024-04-30 22:38:58 +0530

    directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Directives';
+  firstName:string=''
+  lastName:string=''
+  email:string=''
+  address:string=''
+
+  myArray:Array<any>=[]
+  // myArray:Array<string>=['1','2','3','4','5']
+
+  // onAdd(){
+  //   this.myArray.push('6')
+  // }
+  // onDelete(index:number){
+  //   this.myArray.splice(index,1)
+  // }
+  onSubmit(){
+    this.myArray.push({
+      firstName:this.firstName,
+      lastName:this.lastName,
+      email:this.email,
+      address:this.address
+    })
+    console.log(this.myArray)
+  }
+  onDelete(index:number){
+    this.myArray.splice(index,1)
+  }
+  
 }
